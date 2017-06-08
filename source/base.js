1749faf42b4e97d8394429004e322d4830338466
Jason Kuhrt <jason.kuhrt@ssense.com>
2017-06-07 22:16:20 -0400

    disable AirBnB whitespace enforcement

--- source/base.js
+++ source/base.js
@@ -13,55 +13,69 @@ module.exports = {
   },
   plugins: ["import", "fp"],
   env: {
-    mocha: true
+    mocha: true,
+    jest: true,
   },
   rules: {
 
-    // Misc
+    // Disable rules handled by prettier
 
-    semi: [1, "never"],
-    quotes: [1, "double"],
-    "comma-dangle": [1, "only-multiline"],
-    "no-multiple-empty-lines": [1, { max: 3 }],
+    "arrow-parens": 0,
+    "indent": 0,
+    "max-len": 0,
+    "no-trailing-spaces": 0,
+    "no-multiple-empty-lines": 0
+    "semi": 0,
+    "quotes": 0,
+    "comma-dangle": 0,
+    "no-multiple-empty-lines": 0
+    "space-before-function-paren": 0,
+    "space-before-blocks": 0,
+    "operator-linebreak": 0,
+    "object-curly-spacing": 0,
+    "no-nested-ternary": 0,
+    "keyword-spacing": 0,
+    "space-infix-ops": 0,
+    "eol-last": 0,
+    "newline-per-chained-call": 0,
+    "padded-blocks": 0,
+    "space-in-parens": 0,
+    "array-bracket-spacing": 0,
+    "comma-style": 0,
 
     // Import
 
-    "import/no-namespace": 1,
     "import/no-commonjs": 1,
     "import/no-extraneous-dependencies": [
       2,
       {
-        devDependencies: [
-          "**/*.test.js",
-          "**/*.spec.js",
-        ],
+        devDependencies: ["**/*.test.js", "**/*.spec.js"],
       },
     ],
     "import/order": [
       1,
       {
-        groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+        ],
         "newlines-between": "never",
       },
     ],
 
     // Functional Programming
 
-    "fp/no-arguments": 2,
-    "fp/no-class": 2,
-    "fp/no-delete": 2,
-    "fp/no-events": 2,
-    "fp/no-get-set": 2,
-    "fp/no-let": 2,
-    "fp/no-loops": 2,
-    "fp/no-this": 2,
-
-    // Disable
-
-    "space-before-function-paren": 0,
-    "operator-linebreak": 0,
-    "object-curly-spacing": 0,
-    "no-nested-ternary": 0,
+    "fp/no-arguments": 1,
+    "fp/no-class": 1,
+    "fp/no-delete": 1,
+    "fp/no-events": 1,
+    "fp/no-get-set": 1,
+    "fp/no-loops": 1,
+    "fp/no-this": 1,
 
     // Consider
 
